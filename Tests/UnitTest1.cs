7ff54d6c880c7d7dc2d3573a4c2470023a9af260
tavikukko <tomi.tavela@gmail.com>
2021-06-21 14:51:11 +0300

    init

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using HolidayPlanner;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Validators validator = new Validators();
+
+            string iDate = "12-12-2021";
+            DateTime oDate = Convert.ToDateTime(iDate);
+            bool result = validator.IsFutureDate(oDate, out string error);
+            Assert.IsTrue(result,
+                   String.Format("Expected for '{0}': true; Actual: {1}",
+                                 iDate, result));
+        }
+    }
+}
